ca9d3dbbd8ec56fd9e1bc27de92ce09af8b207a0
scrunguss <drevil629@gmail.com>
2019-11-15 12:02:51 +0000

    added commenting capability, fixed exponent capability

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import math as m
 
 RAND = -1
+commented = False
 
 def rand():
     global RAND
     randNo = [1804289383,846930886,1681692777,1714636915,1957747793,424238335,719885386,1649760492,596516649,1189641421,1025202362,1350490027,783368690,1102520059,2044897763,1967513926,1365180540,1540383426,3040891721,303455736,35005211,521595368]
     if (RAND == 21):
         RAND = 0
-    RAND += 1
+    else:
+        RAND += 1
     return randNo[RAND]
 
 def addStack(elem):
@@ -30,66 +32,84 @@ def printStack():
         print(m.trunc(stack[i]))
     return
 
-""" def findOffender(elem):
-    for i in range(len(elem)):
-        if not elem[i].isdigit():
-            return elem[i] """
-
-def parseInfix(elem):
-    print("parsing infix...")
-
-
-def performOperation(elem):
-    #invalid operations
-    if not elem in validoperations:
-        return       
-    #read-only operations
-    if elem == 'd':
-        printStack()
-        return
-    if elem == '=':
-        print(m.trunc(stack[-1]))
-        return
-    if elem == 'r':
-        return rand()
-    #write operations
-    if(len(stack)>=2):
-        y = float(stack.pop())
-        x = float(stack.pop())
-    else:
-        print('Stack Underflow')
-        return
-
-    if elem == '*':
-        return checkSat(x*y)
-    if elem == '/':
-        if y == 0:
-            print('divide by 0.')
-            return
-        else:
-            return checkSat(x/y)
-    if elem == '+':
-        return checkSat(x+y)
-    if elem == '-':
-        return checkSat(x-y)
-    if elem == '%':
-        return checkSat(x%y)
-    if elem == '^':
-        return checkSat(x^y)
-
-def acceptInput():
-    while(1):
-        line = input('')
-        for elem in line.split():
-            if(elem.lstrip('-').isdigit()):
-                addStack(checkSat(int(elem)))                
-            else:   
+def parseInfix(input):
+    parsed = []
+    infix = list(input)
+    for i in range(len(infix)):
+        if infix[i].isdigit() and not commented:
+            if infix[i-1] == '-':
+                if infix[i-2] in validoperations:
+                    infix[i] = '-'+infix[i]
+                    infix[i-1] = '0'
+            addStack(checkSat(int(infix[i])))
+    for i,elem in enumerate(reversed(infix)):
+        if not elem.lstrip('-').isdigit():
                 newelem = performOperation(elem)
                 if newelem is not None:
                     addStack(newelem)
                 else:
-                    if not elem in validoperations: 
-                        parseInfix(elem)
+                    if elem not in validoperations:
+                        print('Unrecognized operator or operand ',elem)
+    return
+            
+def performOperation(elem):
+    if not commented:
+        #invalid operations
+        if not elem in validoperations:
+            return       
+        #read-only operations
+        if elem == 'd':
+            printStack()
+            return
+        if elem == '=':
+            print(m.trunc(stack[-1]))
+            return
+        if elem == 'r':
+            return rand()
+        #write operations
+        if(len(stack)>=2):
+            y = float(stack.pop())
+            x = float(stack.pop())
+        else:
+            print('Stack Underflow')
+            return
+
+        if elem == '*':
+            return checkSat(x*y)
+        if elem == '/':
+            if y == 0:
+                print('divide by 0.')
+                return
+            else:
+                return checkSat(x/y)
+        if elem == '+':
+            return checkSat(x+y)
+        if elem == '-':
+            return checkSat(x-y)
+        if elem == '%':
+            return checkSat(x%y)
+        if elem == '^':
+            return checkSat(x**y)
+
+def acceptInput():
+    global commented
+    while(1):
+        line = input('')
+        for i,elem in enumerate(line.split()):
+            if(elem.lstrip('-').isdigit() and not commented):
+                addStack(checkSat(int(elem)))                
+            else:
+                if commented and elem == '#':
+                    commented = 0
+                elif not commented and elem == '#':
+                    commented = 1
+                elif not commented:   
+                    newelem = performOperation(elem)
+                    if newelem is not None:
+                        addStack(newelem)
+                    else:
+                        if not elem in validoperations: 
+                            parseInfix(elem)
     return
 
 validoperations = ['*','/','+','-','^','%','d','r','=']
